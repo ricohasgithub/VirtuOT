e1990e7730c77585304d91d09fe89a18539e4f7f
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-08-19 14:43:00 -0400

    Added Custom Vue.js Component

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Atrium msg = "VirtuOT"/>
+  </div>
+</template>
+
+<script>
+
+  import Atrium from './components/Atrium.vue'
+
+  export default {
+      name: 'App',
+      components: {
+          Atrium
+      }
+  }
+
+</script>
+
+<style></style>
---
+++ client/src/components/Atrium.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <canvas ref = "space" width = "640" height = "480" style = "border: 1px solid black;">
+
+    </canvas>
+  </div>
+</template>
+
+<script>
+
+    import io from "socket.io"
+
+    export default {
+        name: 'Atrium',
+        data() {
+          return {
+              socket: {},
+              context: {},
+              position: {
+                  x: 0,
+                  y: 0
+              }
+          }
+        },
+        created() {
+            this.socket = io("https://localhost:3000");
+        },
+        mounted() {
+            this.context = this.$refs.space.getContext("2d");
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
