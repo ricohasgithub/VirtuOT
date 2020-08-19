18d5c3a7fb8fea519a5f138a28653bc8071f199d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-08-19 19:25:10 -0400

    Added Firebase Dependencies

--- server/app.js
+++ server/app.js
@@ -58,3 +58,16 @@ http.listen(3000, () => {
     console.log("Listening at port 3000...");
 });
 
+/*
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.18.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.18.0/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+
+*/
