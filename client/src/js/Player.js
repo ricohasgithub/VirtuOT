d94108e8d970793b95d8ff3b5b9843ee6b2c8e1d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-08-19 22:38:26 -0400

    Added Canvas Drawing Code

--- client/src/js/Player.js
+++ client/src/js/Player.js
@@ -5,6 +5,7 @@ export default {
         return {
             socket: {},
             context: {},
+            image: "..//assets/steve.png",
             position: {
                 x: 0,
                 y: 0
@@ -12,19 +13,33 @@ export default {
         }
     },
     created() {
+        // Connect to the socket
         this.socket = io("http://localhost:3000");
+        // Load the avatar image
+        this.avatar = new Image();
+        this.avatar.src = this.image;
+        this.avatar.onload = () => {
+            this.avatar.width = "0.5";
+            this.avatar.height = "0.5";
+        }
     },
     mounted() {
         // Set the context variable to the canvas object (space)
         this.context = this.$refs.space.getContext("2d");
         // Listen for the position message emitted form the server
         this.socket.on("position", data => {
+
             // Set the current position object to the incomming emitted data
             this.position = data;
+
             // Clear the current canvas
             this.context.clearRect(0, 0, this.$refs.space.width, this.$refs.space.height);
-            // Draw the rectangle based off of server-side position data
-            this.context.fillRect(this.position.x, this.position.y, 20, 20);
+
+            this.context.drawImage(this.avatar, this.position.x, this.position.y, 50, 50);
+
+            // // Draw the rectangle based off of server-side position data
+            // this.context.fillRect(this.position.x, this.position.y, 20, 20);
+
         });
     },
     methods: {
